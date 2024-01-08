fff25423a74e648fcf9134d672c7e8d49be5b20f
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-01-08 20:58:33 +0000

    h

--- Desconto Externo Militar.py
+++ Desconto Externo Militar.py
@@ -1,1 +1,38 @@
-hello
+import re
+import pandas as pd
+import fitz
+import io
+import tempfile
+import streamlit as st
+import os
+from datetime import datetime
+# Adiciona um título à barra lateral
+st.sidebar.title("Descontos Externos")
+
+# Adiciona um link para o outro aplicativo Streamlit
+link_militar = "[Militar](/workspaces/streamlit-example/streamlit_app_mil.py)"
+link_civil = "[Civil](/workspaces/streamlit-example/streamlit_app.py)"
+
+st.sidebar.markdown(link_militar, unsafe_allow_html=True)
+st.sidebar.markdown(link_civil, unsafe_allow_html=True)
+# URL da imagem
+image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
+
+#Código HTML e CSS para ajustar a largura da imagem para 20% da largura da coluna e centralizar
+html_code = f'<div style="display: flex; justify-content: center;"><img src="{image_url}" alt="Imagem" style="width:8vw;"/></div>'
+
+data_geracao = datetime.now().strftime('%Y-%m-%d')
+data_geracao2 = datetime.now().strftime('%d/%m/%Y')
+
+
+# Exibir a imagem usando HTML
+st.markdown(html_code, unsafe_allow_html=True)
+
+
+# Centralizar o texto abaixo da imagem
+st.markdown("<h1 style='text-align: center; font-size: 1.5em;'>DIRETORIA DE ADMINISTRAÇÃO DA AERONÁUTICA</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; font-size: 1.2em;'>SUBDIRETORIA DE PAGAMENTO DE PESSOAL</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; font-size: 1em; text-decoration: underline;'>PP1 - DIVISÃO DE DESCONTOS</h3>", unsafe_allow_html=True)
+
+# Texto explicativo
+st.write("Desconto Externo Civil - Extração dados PDF SIAPE para SIAFI")
