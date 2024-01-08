c7a719dc63395185796bd59a67808d8474a68395
Alex Camara <camarafa@gmail.com>
2024-01-08 18:53:48 -0300

    hh

--- DescontoExternoMilitar.py
+++ DescontoExternoMilitar.py
@@ -46,12 +46,8 @@ if uploaded_file is not None:
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(uploaded_file.read())
 
-    # Define padrões de regex para extrair informações específicas
-    padrao_inicio = re.compile(r'H01\s+([0-9,.]+)\s+([A-Z0-9\s-]+)\n')
-    padrao_acantus = re.compile(r'([A-Z]{1}[0-9A-Z]+)\s+([0-9,.]+)\s+([A-Z0-9\s-]+)\n')
-
-    # Inicializa uma lista para armazenar os resultados
-    resultados = []
+    # Define um padrão de regex para extrair padrões de três caracteres que começam com uma letra
+    padrao_acantus = re.compile(r'([A-Z]{1}[0-9A-Z]{2})\s+([0-9,.]+)\s+([A-Z0-9\s-]+)\n')
 
     # Lê o arquivo PDF em blocos
     with fitz.open(temp_file.name) as pdf_doc:
@@ -59,20 +55,8 @@ if uploaded_file is not None:
         for pagina_num in range(num_paginas):
             pagina = pdf_doc[pagina_num]
             texto_pagina = pagina.get_text()
-            
-            # Encontrar o padrão "H01" para iniciar a busca
-            inicio_match = padrao_inicio.search(texto_pagina)
-            if inicio_match:
-                valor_inicial, descricao_inicial = inicio_match.groups()
-                resultados.append(("H01", valor_inicial, descricao_inicial))
-                
-                # Extrair padrões subsequentes a partir de "H01"
-                matches = padrao_acantus.findall(texto_pagina[inicio_match.end():])
-                resultados.extend(matches)
 
-    # Cria um DataFrame com as informações extraídas
-    df = pd.DataFrame(resultados, columns=["CX ACANTUS", "Valor", "Descrição"])
-
-    # Exibe o DataFrame
-    st.write("DataFrame gerado a partir dos dados extraídos:")
-    st.write(df)
+            # Extrai as informações usando regex e imprime os resultados
+            matches = padrao_acantus.findall(texto_pagina)
+            for match in matches:
+                st.write(f"Padrão: {match[0]}, Valor: {match[1]}, Descrição: {match[2]}")
