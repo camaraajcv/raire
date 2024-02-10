d6cbd81b4f84d5605d60f047b0d4d64502ef25cd
camaraajcv <camarafa@gmail.com>
2024-02-09 23:34:07 -0300

    ;lklknlkn

--- DescontoExternoMilitar.py
+++ DescontoExternoMilitar.py
@@ -32,21 +32,23 @@ for country, coords in teste.items():
 
 # Função para lidar com cliques no mapa
 def on_click(event):
-    if event.original_event:
-        lat, lon = event.latlng
-        location = geolocator.reverse((lat, lon), exactly_one=True)
-        if location:
-            st.write(f"Você clicou em {location.address}")
-        else:
-            st.write("Não foi possível identificar o país clicado.")
+    lat, lon = event.latlng
+    location = geolocator.reverse((lat, lon), exactly_one=True)
+    if location:
+        st.write(f"Você clicou em {location.address}")
+    else:
+        st.write("Não foi possível identificar o país clicado.")
 
-# Registrar a função on_click para manipular os eventos de clique no mapa
+# Adicionar manipulador de eventos de clique no mapa
 m.add_child(folium.ClickForMarker(popup=None))
 m.add_child(folium.ClickForMarker(popup=None))
 m.add_child(folium.ClickForMarker(popup=None))
 
 # Renderizar o mapa usando streamlit
-folium_static(m, on_click=on_click)
+folium_static(m)
+
+# Registrar a função on_click para manipular os eventos de clique no mapa
+m.add_click_handler(on_click)
 # URL da imagem
 image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
 
