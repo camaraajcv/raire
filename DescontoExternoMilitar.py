a4c13066d4bf67ea6eaf8b2a69ec5caea678ae65
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-02-14 14:50:29 -0300

    Update DescontoExternoMilitar.py

--- DescontoExternoMilitar.py
+++ DescontoExternoMilitar.py
@@ -215,21 +215,22 @@ if selected_country:
         conversion_factor = data[selected_country][selected_post]
         st.warning(f"O fator do posto selecionado é: {conversion_factor}")
         
+        
+        
+        # Seletor para indicar se leva dependentes e quantos
+        numero_dependentes = st.select_slider("Número de dependentes:", options=[0, 1, 2, 3, 4, 5])
         # Inputs de data para data de ida e volta
         start_date = st.date_input("Selecione a data de início da Portaria:", format="DD/MM/YYYY", min_value=date(2024, 1, 1))
         end_date = st.date_input("Selecione a data de término da Portaria:", format="DD/MM/YYYY")
         
         # Verificar se a data de término é igual ou menor que a data atual
         if end_date <= date.today():
-            st.write("VOCÊ PRECISA SELECIONAR O PERÍODO CORRETAMENTE")
+            st.error("Período Incorreto")
         else:
             # Calcular a diferença de dias entre as datas
             delta = end_date - start_date
-            st.write(f"Período de {delta.days} dias")
-        
-        # Seletor para indicar se leva dependentes e quantos
-        numero_dependentes = st.select_slider("Número de dependentes:", options=[0, 1, 2, 3, 4, 5])
-
+            st.warning(f"Período de {delta.days} dias")
+            
         # Calcular a RAIRE se todas as informações forem fornecidas
         if start_date and end_date and numero_dependentes is not None:
             # Seletor de grau hierárquico
